b2db4381762c96324664fad12f83132996fbe445
Lionel Amiguet <lionel@amiguet.eu>
2024-11-05 22:10:34 +0100

    Improved handling of fullscreen modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 jQuery(document).ready(function($) {
-    $contact = $('.contact')
+    $modal = $('.modal')
+    $contact = $('.modal__contact')
+    $lightbox = $('.modal__lightbox')
+
     $('.menu-item-contact').on('click', function(e) {
         e.preventDefault()
-        $contact.addClass('open')
+        $modal.addClass('modal--open')
+        $contact.addClass('modal__contact--selected')
     })
 
-    $contact.on('click', function(e) {
-        $contact.addClass('close')
-        $contact.removeClass('open')
-        setTimeout(() => $contact.removeClass('close'), 500)
+
+    // Close the modal
+    $modal.on('click', function(e) {
+        $modal.addClass('modal--close')
+        $modal.removeClass('modal--open')
+        setTimeout(() => {
+            $modal.removeClass('modal--close')
+            $contact.removeClass('modal__contact--selected')
+            $lightbox.removeClass('modal__lightbox--selected')
+        }, 500)
 
     })
-    $('.contact__modal').on('click', function(e) {
-        e.stopPropagation()
-    })
+    // Don't close if click on modal
+    $('.modal__contact').on('click', function(e) {e.stopPropagation()})
+    $('.modal__lightbox').on('click', function(e) {e.stopPropagation()})
 })
